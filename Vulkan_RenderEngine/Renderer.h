d2771cf3b279469efefba36d8f2cf9b03bf45d40
izzymon <112693678+Isabel-05@users.noreply.github.com>
2026-03-26 16:52:57 +0100

    Update Renderer.h

--- Vulkan_RenderEngine/Renderer.h
+++ Vulkan_RenderEngine/Renderer.h
@@ -1,17 +1,51 @@
 #pragma once
-class Renderer
-{
-	Renderer();
-	~Renderer();
+#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
 
-	void init();
+#include <stdexcept>
+#include <vector>
+
+#include "Utilities.h"
+
+class VulkanRenderer
+{
+public:
+	VulkanRenderer();
+	~VulkanRenderer();
+
+	int init(GLFWwindow* newWindow);
 	void cleanup();
 
+	
 
 private:
 
-	//Create Functions
-	void createInstance();
+	GLFWwindow* window;
 
+	//Vulkan Components
+	VkInstance instance;
+	struct
+	{
+		VkPhysicalDevice physicalDevice;
+		VkDevice logicalDevice;
+	} 
+	mainDevice;
+
+	VkQueue graphicsQueue;
+
+	//Vulkan Functions
+
+	//createFunctions
+	void createInstance();
+	void createLogicalDevice();
+
+	//get Functions
+	void getPhysicalDevice();
+
+	//support Functions
+	bool checkInstanceExtensionSupport(std::vector<const char*> *extensionsToCheck);
+	bool checkDeviceSuitable(VkPhysicalDevice device);
+
+	QueueFamilyIndices getQueueFamilyIndices(VkPhysicalDevice device);
 };
 
